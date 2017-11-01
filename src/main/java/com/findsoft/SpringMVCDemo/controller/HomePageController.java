c20c93e9b5870f0fa5fd3074f925b9d5d495878e
lizhanyang <15938713165.com>
2017-11-01 18:14:27 +0800

    增加了测试redisTemplate的一些代码

--- src/main/java/com/findsoft/SpringMVCDemo/controller/HomePageController.java
+++ src/main/java/com/findsoft/SpringMVCDemo/controller/HomePageController.java
@@ -3,6 +3,8 @@ package com.findsoft.SpringMVCDemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,32 +14,49 @@ import com.findsoft.SpringMVCDemo.entity.BabySongInfo;
 import com.findsoft.SpringMVCDemo.service.RedisService;
 import com.findsoft.SpringMVCDemo.service.SongService;
 
+/**
+ * 
+ * 
+ * @author lzy
+ *
+ */
 @Controller
-
 public class HomePageController {
     @Autowired
     private SongService songService;
-    @Autowired
-    private  RedisService redisService;
-    
-    @RequestMapping(value="/homepage",method=RequestMethod.GET)
-    public String requestHomePage(){
-        return "Home";//查找Home.jsp
+
+    @RequestMapping(value = "/homepage", method = RequestMethod.GET)
+    public String requestHomePage() {
+        return "Home";// 查找Home.jsp
     }
-    /**@ResponseBody   返回指定的convert对象，在webconfig类中进行配置
+
+    /**
+     * @ResponseBody 返回指定的convert对象，在webconfig类中进行配置
      * @return
      */
-    @RequestMapping(value="/songs",method=RequestMethod.GET)
-    public @ResponseBody List<BabySongInfo> getSongList(){
-        List<BabySongInfo> result=songService.getSongList();
+    @RequestMapping(value = "/songs", method = RequestMethod.GET)
+    public @ResponseBody List<BabySongInfo> getSongList() {
+        List<BabySongInfo> result = songService.getSongList();
         return result;
     }
+
     /**
      * 测试redis集成，不返回任何页面
      */
-    @RequestMapping(value="/redisTest",method=RequestMethod.GET)
-    public void RedisTest(){
-       String result =redisService.get("name");
-       System.out.println("result "+result);
+    @RequestMapping(value = "/redisTest", method = RequestMethod.GET)
+    public void RedisTest() {
+        
+        // String result =stringRedisTemplate.opsForValue().get("name");
+        // System.out.println("result "+result);
+
+        // 此处的实体类 BabySongInfo 一定要实现java.io.Serializable,否则不能直接放入redis
+        // BabySongInfo info=songService.getBabySongById(10);
+        // redisTemplate.opsForValue().set("song", info);
+
+        // BabySongInfo song=(BabySongInfo)
+        // redisTemplate.opsForValue().get("song");
+        // System.out.println("song is "+song.getName());
+
+        // redisTemplate.delete("song");
     }
 }
