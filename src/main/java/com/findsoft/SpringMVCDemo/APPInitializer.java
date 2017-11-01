185d79b311fc1110ce2c6ae0c490a07051b713b4
lizhanyang <yeslzyapple@gmail.com>
2017-11-01 18:11:28 +0800

    增加字符过滤器

--- src/main/java/com/findsoft/SpringMVCDemo/APPInitializer.java
+++ src/main/java/com/findsoft/SpringMVCDemo/APPInitializer.java
@@ -1,5 +1,8 @@
 package com.findsoft.SpringMVCDemo;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
@@ -12,7 +15,7 @@ public class APPInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] { RootConfig.class,RedisConfig.class };// 返回数组，也就是rootconfig可以多个，通常是数据库配置以及普通bean配置
+        return new Class<?>[] { RootConfig.class, RedisConfig.class };// 返回数组，也就是rootconfig可以多个，通常是数据库配置以及普通bean配置
     }
 
     @Override
@@ -25,4 +28,14 @@ public class APPInitializer extends AbstractAnnotationConfigDispatcherServletIni
         return new String[] { "/" };
     }
 
+    /* 设置字符集过滤器，如果设置完还有乱码，请查看tomcat的server.xml配置文件
+     * 
+     */
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] { characterEncodingFilter };
+    }
 }
