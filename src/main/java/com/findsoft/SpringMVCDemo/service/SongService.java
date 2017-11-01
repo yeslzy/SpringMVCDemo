bb60a9adf3b988b8ea765b56dee72779cff98550
lizhanyang <15938713165.com>
2017-11-01 18:16:26 +0800

    增删改查分别加入了redis缓存，同时使用注解以及代码方式。

--- src/main/java/com/findsoft/SpringMVCDemo/service/SongService.java
+++ src/main/java/com/findsoft/SpringMVCDemo/service/SongService.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +21,17 @@ public class SongService {
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
+    /**修改对象，同时删除redis中的缓存
+     * 自定义key会只删除id对应的缓存对象，否则会删除全部缓存，不是我们想要的
+     *
+     * @param id
+     * @param name
+     * @return
+     */
+    @CacheEvict(value="getTheSong",key="#p0")
     public int updateBabySong(int id, String name) {
         int result = 0;
         String sql = "update baby_song_info set name=? where id= ?";
@@ -25,7 +39,14 @@ public class SongService {
 
         return result;
     }
-
+    
+    @CacheEvict(value="getTheSong",key="#p0")
+    public int deleteTheSong(int id) {
+        int result = 0;
+        String sql = "delete from  baby_song_info  where id= ?";
+        result = jdbcTemplate.update(sql, new Object[] { id });
+        return result;
+    }
     public int addNewBabySong(String songName, String authorName, String singer, String country) {
         int result = 0;
         String sql = "insert into baby_song_info(name,author,singer,country,is_foreign,is_recommend,file_url,cover_img_url,tdesc,type) values(?,?,?,?,'0','1','','','','1')";
@@ -33,22 +54,40 @@ public class SongService {
         return result;
     }
 
+    /**
+     * 可以使用注解引入缓存，也可以使用redisTemplate,用起来不要太爽。且能单独设置key的失效时间，不受默认失效时间的限制
+     * @return
+     */
     public List<BabySongInfo> getSongList() {
-        List<BabySongInfo> result = new ArrayList<BabySongInfo>();
-        String sql = "select * from baby_song_info  order by create_date desc";
-        List list = jdbcTemplate.queryForList(sql);
-        if (list != null && list.size() > 0) {
-            for (int i = 0; i < list.size(); i++) {
-                Map map = (Map) list.get(i);
-                BabySongInfo babySongInfo = convertMap2Object(map);
-                result.add(babySongInfo);
+        String cacheKey="babySongList";
+        @SuppressWarnings("unchecked")
+        List<BabySongInfo>result=(List<BabySongInfo>) redisTemplate.opsForValue().get(cacheKey);
+        if(result==null){
+            result = new ArrayList<BabySongInfo>();
+            String sql = "select * from baby_song_info  order by create_date desc";
+            List list = jdbcTemplate.queryForList(sql);
+            System.out.println("query songList from db");
+            if (list != null && list.size() > 0) {
+                for (int i = 0; i < list.size(); i++) {
+                    Map map = (Map) list.get(i);
+                    BabySongInfo babySongInfo = convertMap2Object(map);
+                    result.add(babySongInfo);
+                }
             }
+            redisTemplate.opsForValue().set("babySongList", result, 1, TimeUnit.MINUTES);
         }
+        
         return result;
     }
+    /**根据id获取对象，并放入redis缓存中，下次获取相同id的对象时，直接从缓存中取
+     * @param id
+     * @return
+     */
+    @Cacheable(value="getTheSong",key="#p0")
     public BabySongInfo getBabySongById(int id){
         String sql=" select * from baby_song_info where id=? ";
         Map map=jdbcTemplate.queryForMap(sql, new Object[]{id});
+        System.out.println("query from db");
         return this.convertMap2Object(map);
     }
     private BabySongInfo convertMap2Object(Map map) {
